4d952ebec94fbe86ba137c5ff909c4a724039c32
lskq <lskq012025@gmail.com>
2025-04-07 10:56:32 +0200

    Add event printing

--- Model/Definitions.cs
+++ Model/Definitions.cs
@@ -1,31 +1,96 @@
+using MIDILib.Chunks;
+
 namespace MIDIReader.Model;
 
 public static class Definitions
 {
-    public static string GetFormat(int f)
+    public static string ChunkTypeToString(IChunk chunk)
     {
-        if (f == 0)
-            return $"{f} (Single track)";
-        if (f == 1)
-            return $"{f} (One or more sequential tracks)";
-        if (f == 2)
-            return $"{f} (One or more independent tracks)";
+        var type = chunk.GetType();
 
-        return "Undefined";
+        if (type == typeof(HeaderChunk))
+            return "Header Chunk";
+        if (type == typeof(TrackChunk))
+            return "Track Chunk";
+        if (type == typeof(AlienChunk))
+            return "Alien Chunk";
+
+        return "Unimplemented Chunk";
     }
 
-    public static string GetDivision(int d)
+    public static string HeaderFormatToString(int format)
     {
-        if (d < 0x80)
+        string str = $"{format} ";
+
+        if (format == 0)
+            str += "(Single track)";
+        if (format == 1)
+            str += "(One or more sequential tracks)";
+        if (format == 2)
+            str += "(One or more independent tracks)";
+        else
+            str += "(Undefined)";
+
+        return str;
+    }
+
+    public static string HeaderDivisionToString(int division)
+    {
+        if (division < 0x80)
         {
-            return $"{d} (Ticks per quarter note)";
+            return $"{division} (Ticks per quarter note)";
         }
         else
         {
-            int smpte = (d - 0x80) >> 8;
-            int tpf = d - 0x80 - (smpte << 8);
+            int smpte = (division - 0x80) >> 8;
+            int tpf = division - 0x80 - (smpte << 8);
 
             return $"{smpte} / {tpf} (Negative SMPTE / Ticks per frame)";
         }
+    }
+
+    public static string MIDIEventStatusToString(int code)
+    {
+        int statusByte = code >> 4;
+
+        string midiEventString = statusByte switch
+        {
+            0x8 => "Note Off",
+            0x9 => "Note On",
+            0xA => "Polyphonic Key Pressure",
+            0xB => "Control Change",
+            0xC => "Program Change",
+            0xD => "Channel Pressure",
+            0xE => "Pitch Bend Change",
+            -1 => "Running Status",
+            _ => throw new ArgumentException($"0x{code:X} is not a MIDI Channel status byte."),
+        };
+
+        return midiEventString + $" (0x{statusByte:X1}n)";
+    }
+
+    public static string MetaEventTypeToString(int code)
+    {
+        string metaEventString = code switch
+        {
+            0x00 => "Sequence Number",
+            0x01 => "Text Event",
+            0x02 => "Copyright Notice",
+            0x03 => "Sequence/Track Name",
+            0x04 => "Instrument Name",
+            0x05 => "Lyric",
+            0x06 => "Marker",
+            0x07 => "Cue Point",
+            0x20 => "MIDI Channel Prefix",
+            0x2F => "End of Track",
+            0x51 => "Set Tempo",
+            0x54 => "SMPTE Offset",
+            0x58 => "Time Signature",
+            0x59 => "Key Signature",
+            0x7F => "Sequencer-Specific Meta-Event",
+            _ => "Undefined",
+        };
+
+        return metaEventString + $" (0x{code:X2})";
     }
 }
--- Model/Reader.cs
+++ Model/Reader.cs
@@ -1,11 +1,99 @@
 using MIDILib;
 using MIDILib.Chunks;
 using MIDILib.Events;
+using System.Text;
 
 namespace MIDIReader.Model;
 
 public static class Reader
 {
+    public static void PrintMIDI(MIDIFile midi)
+    {
+        for (int i = 0; i < midi.Chunks.Length; i++)
+        {
+            var chunk = midi.Chunks[i];
+
+            Console.WriteLine($"{i} {Definitions.ChunkTypeToString(chunk)}\n");
+
+            if (chunk.GetType() == typeof(TrackChunk))
+            {
+                var trackChunk = (TrackChunk)chunk;
+
+                for (int j = 0; j < trackChunk.Events.Length; j++)
+                {
+                    var ev = trackChunk.Events[j];
+
+                    string eventCount = $"\tEvent #{j} - ";
+
+                    if (ev.GetType() == typeof(MIDIEvent))
+                    {
+                        Console.WriteLine(eventCount + "MIDI Channel");
+                        PrintMIDIEvent((MIDIEvent)ev);
+                    }
+                    else if (ev.GetType() == typeof(MetaEvent))
+                    {
+
+                        Console.WriteLine(eventCount + "Meta");
+                        PrintMetaEvent((MetaEvent)ev);
+                    }
+                    else if (ev.GetType() == typeof(SysexEvent))
+                    {
+                        Console.WriteLine(eventCount + "Sysex");
+                        PrintSysexEvent((SysexEvent)ev);
+                    }
+                    else
+                    {
+                        Console.WriteLine(eventCount + "Unrecognized");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    public static void PrintMIDIEvent(MIDIEvent midi)
+    {
+        string statusString = Definitions.MIDIEventStatusToString(midi.StatusByte);
+
+        string message = "";
+        foreach (var b in midi.DataBytes)
+        {
+            message += $"0b{b:B8} ";
+        }
+
+        Console.WriteLine($"""
+                Status: {statusString}
+                Channel: {midi.StatusByte & 0xF:X1}
+                Message: {message}
+        """);
+    }
+
+    public static void PrintMetaEvent(MetaEvent meta)
+    {
+        string typeString = Definitions.MetaEventTypeToString(meta.TypeByte);
+        int length = meta.Length;
+
+        string message = Encoding.ASCII.GetString(meta.DataBytes);
+
+        Console.WriteLine($"""
+                Type: {typeString}
+                Length: {length}
+                Message: {message}
+        """);
+    }
+
+    public static void PrintSysexEvent(SysexEvent sysex)
+    {
+        int length = sysex.Length;
+        string message = Encoding.ASCII.GetString(sysex.DataBytes);
+
+        Console.WriteLine($"""
+                Length: {length}
+                Message: {message}
+        """);
+    }
+
     public static void PrintSummary(MIDIFile midi)
     {
         int format = 0;
@@ -63,19 +151,19 @@ public static class Reader
         }
 
         Console.WriteLine($"""
-            Format: {Definitions.GetFormat(format)}
-            Ntrks: {ntrks}
-            Division: {Definitions.GetDivision(division)}
-            
-            Chunks: {nChunks}
-            └ Headers: {nHeaders}
-            └ Tracks: {nTracks}
-            └ Aliens: {nAliens}
-            
-            Events: {nEvents}
-            └ MIDI: {nMIDI}
-            └ Sysex: {nSysex}
-            └ Meta: {nMeta}
+        Format: {Definitions.HeaderFormatToString(format)}
+        Ntrks: {ntrks}
+        Division: {Definitions.HeaderDivisionToString(division)}
+        
+        Chunks: {nChunks}
+        └ Headers: {nHeaders}
+        └ Tracks: {nTracks}
+        └ Aliens: {nAliens}
+        
+        Events: {nEvents}
+        └ MIDI: {nMIDI}
+        └ Sysex: {nSysex}
+        └ Meta: {nMeta}
         """);
     }
 }
